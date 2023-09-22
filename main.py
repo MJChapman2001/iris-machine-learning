8d41e44ab8c887fecdd750a4fcb7b8df2792dcf7
mjc2001 <mjchapman2001@gmail.com>
2023-09-22 23:11:07 +0100

    Summarise the dataset

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ from sklearn.svm import SVC
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv(url, names=names)
+
+print(dataset.shape)
+print(dataset.head(20))
+print(dataset.describe())
